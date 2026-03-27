753d7bd72093a467a58177b4d4507a854f08c391
Thats private <divesh.jadhwani.vf@sse.ac.in>
2026-03-27 21:40:07 +0530

    Add files via upload

---
+++ MOVIES DB SQL TEST/Sql test.sql
@@ -0,0 +1,60 @@
+## movies table ----------------------------------------------------------
+
+
+# select all movies
+
+
+# Select a specific column from movies
+
+
+# Select all bollywood movies
+
+
+# Count how many rows we have in hollywood
+
+
+# Get unique studio names 
+
+
+# Get unique industry names
+
+
+# Show the movies whose rating is more less than 8 and more than 5
+
+
+# demonstrate using having clause
+
+
+## Print  movies  according to imdb_rating
+
+
+
+#### Generate a table like this
+##                                        HOLLYWOOD = 18
+##                                        BOLLYWOOD = 16
+##                                        TOLLYWOOD = 10 
+
+
+
+
+## financicals table ----------------------------------------------------------
+
+
+# calculate , profit = revenue - budget 
+
+
+
+## lets do joinsss
+## Join movies and financials tables-----> Demonstrate ----> Inner, left , right , UNION Joins 
+
+
+
+## lastly showw : 
+
+#                           |  movie_id | title | actor name             |
+#						    |  101      | kgf   |     yash , sanjay dutt |
+
+
+## optional , but can you also demonstrate a sub query 
+
+
